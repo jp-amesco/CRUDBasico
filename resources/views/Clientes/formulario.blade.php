6a52dea82c14b09f6dd04199810f411523d6b8f8
jp-amesco <joaopauloamesco@gmail.com>
2018-06-08 15:44:09 -0300

    Adicionando Crud básico

---
+++ resources/views/Clientes/formulario.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                	Informe as informações dos clientes
+                	<a class="pull-right" href="{{ url('clientes')}}">Listagem de clientes</a>
+                </div>
+
+                <div class="card-body">
+                	@if(Session::has('mensagem_sucesso'))
+                		<div class="alert alert-sucess">{{ Session::get('mensagem_sucesso')}}</div>
+                	@endif
+
+                	@if(Request::is('*/editar'))
+                		{!!Form::model($cliente, ['method' => 'PATCH', 'url' => 'clientes/'.$cliente->id]) !!}
+                	@else
+                		{!!Form::open(['url' => 'clientes/salvar']) !!}
+                	@endif	
+
+                   
+
+                   {!! Form::label('nome', 'Nome') !!}
+                   {!! Form::input('text', 'nome', null, ['classe' => 'form-control', 'autofocus','plcaeholder' => 'Nome']) !!}
+
+                   {!! Form::label('endereco', 'Endereço') !!}
+                   {!! Form::input('text', 'endereco', null, ['classe' => 'form-control', '','plcaeholder' => 'Endereço']) !!}
+
+                   {!! Form::label('numero', 'Número') !!}
+                   {!! Form::input('text', 'numero', null, ['classe' => 'form-control', '','plcaeholder' => 'numero']) !!}
+
+                   {!! Form::submit('Salvar', ['class'=>'btn btn-primary'] ) !!}
+
+                   {!!Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/Clientes/lista.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                	Informe as informações dos clientes
+                	<a class="pull-right" href="{{ url('clientes/novo')}}">Novo Clientes</a>
+                </div>
+
+                <div class="card-body">
+                	@if(Session::has('mensagem_sucesso'))
+                		<div class="alert alert-sucess">{{ Session::get('mensagem_sucesso')}}</div>
+                	@endif
+                   <table class="table">
+                   		<th>Nome</th>
+                   		<th>Endereço</th>
+                   		<th>Número</th>
+                   		<th>Ações</th>
+                   		<tbody>
+                   			@foreach($clientes as $cliente)
+                   			<tr>
+                   				<td>{{ $cliente->nome }}</td>
+                   				<td>{{ $cliente->endereco }}</td>
+                   				<td>{{ $cliente->numero }}</td>
+                   				<td>
+                   					<a href="clientes/{{ $cliente->id}}/editar" class="btn btn-default btn-sm">Editar</a>
+                   					{!! Form::open(['method' => 'DELETE', 'url' => 'clientes/'.$cliente->id, 'style' => 'display:inline']) !!}
+                   					<button type="submit" class="btn btn-default btn-sm">Excluir</button>
+                   					{!! Form::close() !!}
+                   				</td>
+                   			</tr>
+                   			@endforeach
+                   		</tbody>
+                   </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
